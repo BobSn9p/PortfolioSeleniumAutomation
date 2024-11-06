a3dc2cba1a1ffdaa6fc87653d36cc391b42637d4
BobSn9p <kwiatkowski.patryk@o2.pl>
2024-11-06 01:33:34 +0100

    addItemToCartWithoutChosenSize test refactor and finish

--- src/test/java/tests/SearchAndItemDetailsTests.java
+++ src/test/java/tests/SearchAndItemDetailsTests.java
@@ -3,14 +3,15 @@ package tests;
 import org.testng.annotations.Test;
 import page.objects.*;
 import testData.TestData;
+import waits.WaitForElement;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.*;
 
 
 public class SearchAndItemDetailsTests extends TestBase {
 
     @Test
-    public void addItemToCartWithoutChosenSize() {
+    public void addItemToCartWithoutChosenSize() throws InterruptedException {
         LandingPage landingPage = new LandingPage();
         landingPage
                 .clickOnAcceptCookiesButton()
@@ -18,10 +19,15 @@ public class SearchAndItemDetailsTests extends TestBase {
         String searchedProductName = landingPage.getProductNameAfterSearch();
         assertEquals(searchedProductName, TestData.HELMET_FULL_NAME_MT_STINGER_2_SOLID_MATT_BLACK);
 
-        HelmetDetailPage helmetDetailPage = new HelmetDetailPage();
-        helmetDetailPage.clickOnHelmetDetails();
+        HelmetDetailPage helmetDetailPage = new HelmetDetailPage()
+                .clickOnHelmetDetails();
 
         assertEquals(helmetDetailPage.getHelmetSizes(), TestData.SIZES_5);
+
+        boolean isAlertDisplayed = helmetDetailPage
+                .clickOnAddToCartButton()
+                .isChooseSizeAlertDisplayed();
+        assertTrue(isAlertDisplayed);
 
     }
 
