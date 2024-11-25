9fdcf41f2a39814500c44d43c9940d33ae2f77dc
BobSn9p <kwiatkowski.patryk@o2.pl>
2024-11-25 23:41:28 +0100

    Added tags and second test runner

---
+++ src/test/java/runners/SmokeTestsRunner.java
@@ -0,0 +1,23 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/Features",
+        glue = {"stepDefinitions"},
+        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+        monochrome = true,
+        tags = "@smoke"
+)
+public class SmokeTestsRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        int threadCount = Integer.parseInt(System.getProperty("threadCount", "4"));
+        System.setProperty("dataproviderthreadcount", String.valueOf(threadCount));
+        return super.scenarios();
+    }
+}
